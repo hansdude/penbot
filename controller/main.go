507ea670c8d817e0a234a6fab7a5c3efcddc49ce
Hans-Peter Klett <hans@hansdude.com>
2017-09-10 14:03:04 -0600

    go fmt

--- controller/main.go
+++ controller/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"flag"
 	"log"
-  "flag"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -13,22 +13,21 @@ var x float64
 var y float64
 
 func main() {
-  flag.Float64Var(&x, "x", 20.0, "The x coordinate to send to the server.")
-  flag.Float64Var(&y, "y", 90.0, "The y coordinate to send to the server.")
-  flag.Parse()
+	flag.Float64Var(&x, "x", 20.0, "The x coordinate to send to the server.")
+	flag.Float64Var(&y, "y", 90.0, "The y coordinate to send to the server.")
+	flag.Parse()
 
-  requestPoint := pb.Point{X:x, Y:y}
+	requestPoint := pb.Point{X: x, Y: y}
 
-  conn, err := grpc.Dial("ev3dev.local:4321", grpc.WithInsecure())
-  if err != nil {
-    log.Fatalf("could not connect: %v", err)
-  }
-  defer conn.Close()
-  c := pb.NewPenBotClient(conn)
+	conn, err := grpc.Dial("ev3dev.local:4321", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewPenBotClient(conn)
 
-  _, err = c.EnqueuePosition(context.Background(), &pb.EnqueuePositionRequest{P:&requestPoint})
-  if err != nil {
-    log.Fatalf("could not enqueue position: %v", err)
-  }
+	_, err = c.EnqueuePosition(context.Background(), &pb.EnqueuePositionRequest{P: &requestPoint})
+	if err != nil {
+		log.Fatalf("could not enqueue position: %v", err)
+	}
 }
-
