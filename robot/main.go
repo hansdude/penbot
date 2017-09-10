0cc467a7e8924497df2d47169c60546f1484872c
Hans-Peter Klett <hans@hansdude.com>
2017-09-10 14:01:38 -0600

    connect the service to the motors

--- robot/main.go
+++ robot/main.go
@@ -10,6 +10,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	pb "penbot/shared"
+	"github.com/ev3go/ev3dev"
 )
 
 const (
@@ -39,20 +40,77 @@ func right_angle(p *pb.Point, config *Config) float64 {
 	return find_angle(&pb.Point{X: p.X - config.A, Y: p.Y}, config.L, config.R, Right)
 }
 
+func to_motor_position(angle float64) int {
+  // Go from radians to degrees.
+  scaled := (angle / math.Pi) * 180.0
+  //TODO: maybe change this to be an initialization step
+  // Rotate by -90*
+  shifted := (scaled - 90.0)
+  // Compensate for the 3:1 gear ratio.
+  gearScaled := shifted * 3.0
+  // Enforce -195 lower limit 40 upper limit.
+  if gearScaled < -195.0 {
+    log.Printf("Position %f was below lower limit -195", gearScaled)
+    return -195
+  }
+  if gearScaled > 40.0 {
+    log.Printf("Position %f was above upper limit 40", gearScaled)
+    return 40
+  }
+  return int(math.Floor(gearScaled + .5))
+}
+
 type Server struct {
   Config *Config
+  Left *Motor
+  Right *Motor
 }
 
 func (s *Server) EnqueuePosition(
 	ctx context.Context, request *pb.EnqueuePositionRequest) (*pb.EnqueuePositionResponse, error) {
-	fmt.Println("Position: (%f, %f)", request.P.X, request.P.Y)
 	a1 := left_angle(request.P, s.Config)
 	a2 := right_angle(request.P, s.Config)
-	fmt.Println(a1)
-	fmt.Println(a2)
+  leftMotorPosition := to_motor_position(a1)
+  rightMotorPosition := to_motor_position(a2)
+	fmt.Println("Position: (%f, %f)", request.P.X, request.P.Y)
+	fmt.Println("Left angle: %f", a1)
+	fmt.Println("Right angle: %f", a2)
+	fmt.Println("Left motor position: %d", leftMotorPosition)
+	fmt.Println("Right motor position: %d", rightMotorPosition)
+  s.Left.SetPosition(leftMotorPosition)
+  s.Right.SetPosition(rightMotorPosition)
+  s.Left.Go()
+  s.Right.Go()
 	return &pb.EnqueuePositionResponse{}, nil
 }
 
+type Motor struct {
+  Ev3Motor *ev3dev.TachoMotor
+}
+
+func InitMotor(output string) *Motor {
+	motor, err := ev3dev.TachoMotorFor(output, "lego-ev3-l-motor")
+	if err != nil {
+    log.Fatalf("count not get motor %s: %v", output, err)
+	}
+	err = motor.SetStopAction("brake").Err()
+	if err != nil {
+    log.Fatalf("could not set stop action: %v", err)
+	}
+	motor.SetSpeedSetpoint(100)
+  return &Motor{Ev3Motor:motor}
+}
+
+func (motor *Motor) SetPosition(position int) {
+	motor.Ev3Motor.SetPositionSetpoint(position)
+}
+
+func (motor *Motor) Go() {
+	motor.Ev3Motor.Command("run-to-abs-pos")
+}
+
+// x: -70mm min, 70mm max
+// y: 70mm min, 120mm max
 func main() {
 	config := Config{
 		A: 32, // 32mm
@@ -60,22 +118,22 @@ func main() {
 		R: 80, // 80mm
 	}
 
+	leftMotor := InitMotor("outA")
+	rightMotor := InitMotor("outB")
+
 	lis, err := net.Listen("tcp", ":4321")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterPenBotServer(s, &Server{Config:&config})
+	pb.RegisterPenBotServer(s, &Server{
+    Config: &config,
+    Left: leftMotor,
+    Right: rightMotor,
+  })
 	reflection.Register(s)
 	fmt.Println("serving")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
-
-// TODO:
-// turn off the brake and get the angle limits
-// 3x gear ratio
-// -195 lower limit 40 upper limit
-// x: -70mm min, 70mm max
-// y: 70mm min, 120mm max
