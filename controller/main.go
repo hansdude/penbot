274ec0cfb025a6c5e3f176c86d157da1e12f80de
Hans-Peter Klett <hans@hansdude.com>
2017-09-10 13:15:24 -0600

    Make client controllable via command line

--- controller/main.go
+++ controller/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"log"
+  "flag"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	pb "penbot/shared"
 )
 
+var x float64
+var y float64
+
 func main() {
+  flag.Float64Var(&x, "x", 20.0, "The x coordinate to send to the server.")
+  flag.Float64Var(&y, "y", 90.0, "The y coordinate to send to the server.")
+  flag.Parse()
+
+  requestPoint := pb.Point{X:x, Y:y}
+
   conn, err := grpc.Dial("ev3dev.local:4321", grpc.WithInsecure())
   if err != nil {
     log.Fatalf("could not connect: %v", err)
@@ -16,7 +26,7 @@ func main() {
   defer conn.Close()
   c := pb.NewPenBotClient(conn)
 
-  _, err = c.EnqueuePosition(context.Background(), &pb.EnqueuePositionRequest{P:&pb.Point{X: 20, Y: 90}})
+  _, err = c.EnqueuePosition(context.Background(), &pb.EnqueuePositionRequest{P:&requestPoint})
   if err != nil {
     log.Fatalf("could not enqueue position: %v", err)
   }
